59df9f8169a1ec0575f2ac76dc37ba349887f393
dmuchai <dmmuchai@gmail.com>
2025-05-15 21:25:36 +0300

    Task 0 UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,32 +1,36 @@
 #!/usr/bin/python3
-"""
-UTF-8 Validation
-"""
+"""UTF-8 Validation"""
+def get_leading_set_bits(num):
+    """Returns the number of leading set bits (1s) in a byte."""
+    set_bits = 0
+    helper = 1 << 7
+    while helper & num:
+        set_bits += 1
+        helper = helper >> 1
+    return set_bits
 
 def validUTF8(data):
     """Determines if a given data set represents a valid UTF-8 encoding."""
-    num_bytes = 0
+    bits_count = 0
 
-    for byte in data:
-        # Only consider the 8 least significant bits
-        byte = byte & 0xFF
+    for i in range(len(data)):
+        byte = data[i] & 0xFF
 
-        if num_bytes == 0:
-            # Count the number of leading 1's
-            if (byte >> 7) == 0:
+        if bits_count == 0:
+            bits_count = get_leading_set_bits(byte)
+
+            # 1-byte character (0xxxxxxx)
+            if bits_count == 0:
                 continue
-            elif (byte >> 5) == 0b110:
-                num_bytes = 1
-            elif (byte >> 4) == 0b1110:
-                num_bytes = 2
-            elif (byte >> 3) == 0b11110:
-                num_bytes = 3
-            else:
+
+            # UTF-8 characters must be between 1 and 4 bytes
+            if bits_count == 1 or bits_count > 4:
                 return False
         else:
-            # Check that the byte starts with 10xxxxxx
-            if (byte >> 6) != 0b10:
+            # Check for continuation byte format: 10xxxxxx
+            if not (byte & (1 << 7) and not (byte & (1 << 6))):
                 return False
-            num_bytes -= 1
 
-    return num_bytes == 0
+        bits_count -= 1
+
+    return bits_count == 0
